88efda523e2a4e40d9857f30d011bd0d8d231717
Mehmetali Shaqiri <mehmetalishaqiri@gmail.com>
2015-03-25 17:19:29 +0100

    call api from node.js app

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,43 @@
 ﻿var express = require('express');
 var router = express.Router();
+var http = require("http");
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: "What's My Reputation?" });
+    var opts = {
+        host: "api.whatsmyrepo.crea-ko.com",
+        port: 80,
+        path: "/api/reputation",
+        method: "GET",
+        headers : {
+            "User-Agent": "What's My Rep"
+        }
+    }
+    
+    http.get(opts, function (response) {
+        
+        var buffer = "", 
+            data;
+        
+        response.on("data", function (chunk) {
+            buffer += chunk;
+        });
+        
+        response.on("end", function (err) {
+            
+            data = JSON.parse(buffer);
+            res.render('index', {
+                title: "What's My Reputation?", 
+                developers : data
+            });
+        
+        });
+    }).on('error', function (error) {
+        res.render('index', {
+            title: "What's My Reputation?", 
+            developers : []
+        });
+    });
 });
 
 module.exports = router;
